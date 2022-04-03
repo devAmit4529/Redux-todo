5e2cbbbb93a4e8bc965fdd7458798e5649076ae7
amit <amit.kumar@antiersolutions.com>
2022-04-03 12:47:11 +0530

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo,deleteTodo,removeTodo } from '../actions';
+const Todo = () => {
+
+    const [inputData, setInputData] = useState ('');
+    const list = useSelector((state) => state.todoReducers.list );
+    const dispatch = useDispatch();
+    return(
+        <>
+            <div className='main-div'>
+        <div className='child-div'>
+            <figure>
+                <figcaption> Add your list HeRE ✌️</figcaption>
+            </figure>
+        <div className='addItems'>
+            <input type='text' placeholder='Add ItEMS✍️'
+            value={inputData} 
+            onChange = { (event) =>setInputData(event.target.value)}/>
+            <i className="fa fa-plus add-btn" onClick={ () =>dispatch(addTodo(inputData), setInputData('')) }></i>
+        </div>
+                <div className='showItems'>
+                    {
+                        list.map((elem) => {
+                            return(
+                            <div className='eachItem' key={elem.id}>
+                                <h3> {elem.data} </h3>
+                                <div className='todo-btn'>
+                                <i class='far fa-trash-alt add-btn' title='delete item' onClick={ () =>dispatch(deleteTodo(elem.id)) }></i>
+                            </div>
+                            </div>
+                            )
+                        })
+                    }
+                    
+                </div>
+                    
+                <div className='showItems'>
+                        <button className='btn effect04' data-sm-link-text= 'remove all' 
+                        onClick={()=> dispatch(removeTodo())}><span>Check List</span></button>
+                </div>
+        </div>
+        </div>
+        </> 
+    )
+}
+export default Todo;
